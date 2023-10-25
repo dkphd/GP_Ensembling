3fc69a845353104b179f8ee39f619dc2f84e0172
damiankucharski <c.damiankucharski@gmail.com>
2023-10-25 21:15:22 +0200

    add aggregation operator

--- src/tree.py
+++ src/tree.py
@@ -7,6 +7,8 @@ import random
 
 from graphviz import Digraph
 
+from functools import partial
+
 
 class Node:
     def __init__(self, parent=None, children=[], tree=None):
@@ -47,6 +49,39 @@ class BinaryOperatorNode(OperatorNode):
         return f"BinaryOperatorNode: {self.operator}"
 
 
+class ReductionOperatorNode(OperatorNode):
+    def __init__(self, parent, children, tree):
+        super().__init__(parent, children, tree)
+        self.operator = lambda x: None
+
+    def calculate(self):
+        parent_eval = self.parent.evaluation if self.parent.evaluation is not None else self.parent.value
+
+        concat = Tensor.stack([parent_eval] + [child.calculate() for child in self.children], dim=0)
+        return self.operator(concat)
+
+
+class MeanNode(ReductionOperatorNode):
+    def __init__(self, parent, children, tree):
+        super().__init__(parent, children, tree)
+        self.operator = partial(Tensor.mean, axis=0)
+
+    def __str__(self):
+        return f"MeanNode"
+
+
+class WeightedMeanNode(MeanNode):
+    def __init__(self, parent, children, tree, weights: Tensor):
+        super().__init__(parent, children, tree)
+
+        assert len(weights.shape) == 2
+        assert weights.shape[0] == 1
+        assert weights.shape[1] == len(children) + 1
+
+        self.weights = weights
+        self.operator = lambda x: super().operator(x * self.weights)
+
+
 class AddNode(BinaryOperatorNode):
     def __init__(self, parent, children, tree):
         super().__init__(parent, children, tree)
@@ -69,16 +104,6 @@ class DivNode(BinaryOperatorNode):
     def __init__(self, parent, children, tree):
         super().__init__(parent, children, tree)
         self.operator = truediv
-
-
-class WeightedMeanNode(BinaryOperatorNode):
-    def __init__(self, parent, children, tree, weight_top=0.5):
-        super().__init__(parent, children, tree)
-        self.weight_top = weight_top
-        self.operator = lambda x, y: self.weight_top * x + (1 - self.weight_top) * y
-
-    def __str__(self):
-        return f"WeightedMeanNode with weight: {self.weight_top}"
 
 
 class ValueNode(Node):
