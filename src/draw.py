3b5aae662a8a411dfd38ed9c68c67cad515e7e99
damiankucharski <c.damiankucharski@gmail.com>
2023-10-27 18:09:52 +0200

    fix imports in drawing

--- src/draw.py
+++ src/draw.py
@@ -1,12 +1,13 @@
 from graphviz import Digraph
 
-from src.node import ValueNode
+from src.node import ValueNode, Node
+from src.tree import Tree
 
 import numpy as np
 
-from typing import Any
+from typing import Union
 
-def draw_tree(to_draw: Any[Tree, Node], dot=None):
+def draw_tree(to_draw: Union[Tree, Node], dot=None):
 
     if isinstance(to_draw, Tree):
         node = to_draw.root
@@ -17,11 +18,11 @@ def draw_tree(to_draw: Any[Tree, Node], dot=None):
         dot = Digraph(comment="Tree")
 
     if isinstance(node, ValueNode):
-        value = node.value.numpy() if (np.prod(node.value.shape) <= 9) else f"Tensor with memory adress: {id(node.value)}"
+        value = node.value.numpy() if (np.prod(node.value.shape) <= 9) else f"Tensor with memory adress: {hex(id(node.value))}"
 
         
         if node.evaluation is not None:
-            evaluation = node.evaluation.numpy() if (np.prod(node.evaluation.shape) <= 9) else f"Tensor with memory adress: {id(node.evaluation)}"
+            evaluation = node.evaluation.numpy() if (np.prod(node.evaluation.shape) <= 9) else f"Tensor with memory adress: {hex(id(node.evaluation))}"
         else:
             evaluation = None
 
@@ -31,14 +32,14 @@ def draw_tree(to_draw: Any[Tree, Node], dot=None):
         display_string += f"Value: {value} | Eval: {evaluation}"
 
         dot.node(
-            f"{id(node)}",
+            f"{hex(id(node))}",
             display_string,
         )
     else:
-        dot.node(f"{id(node)}", f"Op\n{node.operator}")
+        dot.node(f"{hex(id(node))}", f"Op\n{node.operator}")
 
     for child in node.children:
         draw_tree(child, dot)
-        dot.edge(f"{id(node)}", f"{id(child)}")
+        dot.edge(f"{hex(id(node))}", f"{hex(id(child))}")
 
     return dot
