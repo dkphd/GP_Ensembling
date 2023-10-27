82b2d9a0977e0b618e5b12120dfd5f5a46bd084d
damiankucharski <c.damiankucharski@gmail.com>
2023-10-27 18:01:21 +0200

    clear gp to be used further as base file for running out of the box

--- gp.py
+++ gp.py
@@ -1,13 +1,2 @@
-from src.tree import *
-
 if __name__ == "__main__":
-    tree = Tree()
-    tree.grow_tree(tree.root, max_depth=3)
-    dot = draw_tree(tree.root)
-    dot.render("tree", view=False, format="png")
-
-    # Calculate the tree
-    print(tree.root.calculate())
-    dot = draw_tree(tree.root)
-    tree.root.evaluation.numpy()
-    dot.render("tree2", view=False, format="png")
+    pass
