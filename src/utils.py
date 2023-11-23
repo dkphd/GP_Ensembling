9a4730ee5881ae17701018106c92c6f17d65c23a
damiankucharski <c.damiankucharski@gmail.com>
2023-11-23 15:19:01 +0100

    Pickle util

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import pickle
+
+class Pickle:
+    @staticmethod
+    def load(path):
+        with open(path, "rb") as file:
+            return pickle.load(file)
+
+    @staticmethod
+    def save(path, obj):
+        with open(path, "wb") as file:
+            pickle.dump(obj, file)
