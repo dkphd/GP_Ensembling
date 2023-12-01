6116203c4cc5a17f1559e51d3898589e6e545673
damiankucharski <c.damiankucharski@gmail.com>
2023-12-01 20:25:12 +0100

    remove regeneration, add BCE loss fitness

--- gp.py
+++ gp.py
@@ -32,6 +32,21 @@ def f1_score_fitness(tree: Tree, gt: Tensor):
     return f1_score(gt.numpy(), pred.numpy()) 
 
 
+def binary_cross_entropy_loss(y_true, y_pred): 
+    loss = - (y_true * y_pred.log() + (1 - y_true) * (1-y_pred).log()).mean()
+    return loss
+
+
+def binary_cross_entropy_fitness(tree: Tree, gt: Tensor):
+    pred = tree.evaluation
+    gt = gt.float()
+    return -binary_cross_entropy_loss(gt, pred).numpy().item()
+
+
+
+FITNESS = binary_cross_entropy_fitness
+
+
 def first_uniques(arr):
 
     mask = []
@@ -103,15 +118,16 @@ def choose_pareto_rest_sorted(population, fitnesses, n):
     return population, fitnesses
 
 
-
-def regenerate_population(population, fitnesses, n):
+def regenerate_population(population, fitnesses, n, tensors, gt, ids, population_codes = None):
+    if population_codes is None:
+        population_codes = [tree.__repr__() for tree in population]
     while len(population) < n:
-        new_tree = Tree.create_tree_from_models(tensors, ids = paths)
+        new_tree = Tree.create_tree_from_models(tensors, ids = ids)
         code = new_tree.__repr__()
         if code not in population_codes:
             population.append(new_tree)
             population_codes.append(code)
-            fitnesses = np.concatenate([fitnesses, [f1_score_fitness(new_tree, gt)]])
+            fitnesses = np.concatenate([fitnesses, [FITNESS(new_tree, gt)]])
 
     return population, fitnesses
         
@@ -119,8 +135,8 @@ def regenerate_population(population, fitnesses, n):
 def load_args():
 
     parser = ArgumentParser()
-    parser.add_argument("--input_path", default="./additional_valid_probs")
-    parser.add_argument("--gt_path", default="./to_evolution/additional_valid_y.pt")
+    parser.add_argument("--input_path", default="./train_probs")
+    parser.add_argument("--gt_path", default="./train_y.pt")
     parser.add_argument("--population_size", type=int, default=20)
     parser.add_argument("--population_multplier", type=int, default=1)
     parser.add_argument("--tournament_size", type=int, default=5)
@@ -138,8 +154,7 @@ if __name__ == "__main__":
 
     print("Generating population...")
 
-    population = [Tree.create_tree_from_models(tensors, ids = paths) for _ in range(population_size)]
-    
+    population, _ = regenerate_population([], [], population_size, tensors, gt, paths, population_codes = None)
 
     ids = np.arange(population_size)
     additional_population = []
@@ -152,7 +167,7 @@ if __name__ == "__main__":
 
         GLOBAL_ITERATION = i
 
-        fitnesses = np.array([f1_score_fitness(tree, gt) for tree in population])
+        fitnesses = np.array([FITNESS(tree, gt) for tree in population])
 
         while len(additional_population) < len(population) * population_multplier:
             idxs = np.random.choice(ids, tournament_size, replace=False)
@@ -191,7 +206,7 @@ if __name__ == "__main__":
                     continue
 
         population += additional_population
-        additional_fitnesses = np.array([f1_score_fitness(tree, gt) for tree in additional_population])
+        additional_fitnesses = np.array([FITNESS(tree, gt) for tree in additional_population])
         fitnesses = np.concatenate([fitnesses, additional_fitnesses])
 
         additional_population = []
@@ -201,29 +216,21 @@ if __name__ == "__main__":
         unique_codes = first_uniques(population_codes)
 
         print("Removing duplicates...")
-        print(f"There is {len(population_codes) - sum(unique_codes)} duplicates in population")    
+        print(f"There are {len(population_codes) - sum(unique_codes)} duplicates in population")    
 
         population = [tree for tree, unique in zip(population, unique_codes) if unique]
         fitnesses = fitnesses[unique_codes]
 
-        # regenearte population
-        population, fitnesses = regenerate_population(population, fitnesses, population_size)
+        print("There is {} unique trees in population".format(len(population)))
 
         population, fitnesses = choose_pareto_rest_sorted(population, fitnesses, population_size)
 
-        print(F"Population size: {len(population)} after selection and before regeneration")
-
-        # regenearte population
-        population, fitnesses = regenerate_population(population, fitnesses, population_size)
-
-
         print("Best fitness:")
-
         print(np.max(fitnesses))
         
         if DEBUG:
             dot = draw_tree(population[np.argmax(fitnesses)])
-            dot.render(f"trees/best_{i}", view=True, format='png')
+            dot.render(f"trees/best_{i}", format='png')
 
     print(fitnesses)
     population[0].save_tree_architecture("./best_tree.tree")
