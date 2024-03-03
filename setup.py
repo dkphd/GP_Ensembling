ab62f1895c74ddee8e005a1dbdcfc2632225c3a0
damiankucharski <c.damiankucharski@gmail.com>
2024-03-03 13:36:09 +0100

    update giraffe version

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='giraffe',
-    version='0.2.0',
-    packages=find_packages(), 
+    name="giraffe",
+    version="0.3.0",
+    packages=find_packages(),
     install_requires=[
-        'numpy',  
-        'pandas',
-        'torch',
-        'matplotlib',
-        'graphviz',
-        'paretoset',
-        'tinygrad @ git+https://github.com/tinygrad/tinygrad', 
+        "numpy",
+        "pandas",
+        "torch",
+        "matplotlib",
+        "graphviz",
+        "paretoset",
+        "tinygrad @ git+https://github.com/tinygrad/tinygrad",
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11'
+    python_requires=">=3.11",
 )
